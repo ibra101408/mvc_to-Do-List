230d78e67ab8d83eb205b30a69fd88dac9baf1dc
ibra101408 <danikvod@gmail.com>
2023-03-10 00:25:34 +0200

    As a user I want to view all my toDo list [#184666476]

--- Controllers/controller.js
+++ Controllers/controller.js
@@ -2,6 +2,17 @@ const express = require('express');
 const router = express.Router();
 const model = require('../models/model');
 
+// GET all to-do items
+router.get('/', async (req, res) => {
+    try {
+        const todos = await model.getAll();
+        res.render('index', { todos });
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+
+});
 
 
 module.exports = router;
--- Models/model.js
+++ Models/model.js
@@ -2,3 +2,18 @@ const sqlite3 = require('sqlite3').verbose();
 
 const db = new sqlite3.Database('identifier.sqlite')
 
+
+// Export functions to interact with the database
+module.exports = {
+    getAll: () => {
+        return new Promise((resolve, reject) => {
+            db.all('SELECT * FROM todos', [], (err, rows) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(rows);
+                }
+            });
+        });
+    },
+};
